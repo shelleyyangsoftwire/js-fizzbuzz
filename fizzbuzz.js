45e60ce9084906ccc85d5e8749f93aa2c40c6851
Shelley Yang <Shelley.Yang@softwire.com>
2025-07-02 09:02:20 +0100

    1st part

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,20 @@
 function fizzbuzz() {
     console.log("Hello, World!");
 
-    // Put your code here...
+    for (var i = 1; i <= 100; i++){
+        txt ='';
+        if (i%3 == 0){
+            txt += 'Fizz';
+        }
+        if (i%5 == 0){
+            txt += 'Buzz';
+        }
+        if (txt==''){
+            console.log(i)
+        } else {
+            console.log (txt);
+        }
+    }
 
 }
 

4c98a9560e2d22a7db0c1f148d34aaec6f941af8
SheYan <shelley.yang@softwire.com>
2025-07-02 17:22:21 +0100

    defining class, general function for checking input validity

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,72 @@
-//Allowing users to specify their own functions would work for 3 - 11,
-// just by allowing them to set via command line the contents of the words array
-// allowing them to specify reordering operations like 17 would be harder, as it is a different
-// type of change not captured in the arrays i've made -- possibly an array of predefined functions
-// attachable to any number will make this easier to change via user command line inputs?
+//Experimental attempt to let the user create whatever rules they want
 
+class multipleRule{
+    constructor(active, number, word, action){
+        this.active = active;
+        this.number = number;
+        this.word = word;
+        this.action = action;
+    }
+}
+
+// eligible actions
+const validActions = ['append', 'prepend', 'reverse'];
+function addtoEnd(text, word){
+    text.push(word);
+}
+function addtoStart(text, word){
+    text.unshift(word);
+}
+function reverse(text){
+    text.reverse();
+}
+
+// check user input is actually workable
+function checkValidInput(answer){
+    if (answer.trim() === ''){
+        console.log ('No input. Try again!');
+        return false;
+    } else {
+        answer = answer.split(';');
+        if (answer.length > 5){
+            console.log ('Too many rules. Try again!');
+            return false;
+        }
+    }
+}
+
+
+const { createInterface } = require('readline');
+//std = standard input/output?
+
+const getInput = createInterface ({
+    input: process.stdin,
+    output: process.stdout
+});
+
+getInput.question (`Enter some rules in the format: \n
+number, word, action; number, word, action; \n\n
+You may enter any positive integer and word. \n
+There are only three actions: Append, Prepend, and Reverse.\n
+Reverse does not add any words -- what you write will be ignored, simply put a space -- eg. 3, , Reverse \n
+Any entries that does not fit the format will also be ignored.`, (answer) =>{
+
+    if (checkValidInput(answer) === false){
+        getInput.close();
+        process.exit();
+    }
+
+    getInput.question ('What is the maximum number you would like FizzBuzz to go to?  ', (answer) =>{
+        //fizzbuzz(answer);
+        getInput.close();
+    });
+
+});
+
+
+
+
+/*
 numbers = [3, 13, 5, 7, 11];
 active = [false, false, false, false, false];
 words = ['Fizz', 'Fezz', 'Buzz', 'Bang', 'Bong'];
@@ -44,13 +107,6 @@ function fizzbuzz(max) {
 
 // readline bit acquired from w3schools
 
-const { createInterface } = require('readline');
-//std = standard input/output?
-
-const getInput = createInterface ({
-    input: process.stdin,
-    output: process.stdout
-});
 
 function makeActive(answer){
     answer = answer.trim();
@@ -88,8 +144,7 @@ getInput.question ('Enter a multiple to process, separated by commas. eg: 3, 5, 
 
 
 
-
-
+*/
 
 
 

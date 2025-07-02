ca07780e240db36a52a6357faf99f7724a605ceb
Shelley Yang <Shelley.Yang@softwire.com>
2025-07-02 10:21:44 +0100

    adding 17

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,7 +5,7 @@ words = ['Fizz', 'Fezz', 'Buzz', 'Bang']
 function fizzbuzz() {
     console.log("Hello, World!");
 
-    for (var i = 1; i <= 105; i++){
+    for (var i = 1; i <= 255; i++){
         txt =[];
         for (var j = 0; j < numbers.length; j++){
             if (i%numbers[j] == 0){
@@ -16,11 +16,15 @@ function fizzbuzz() {
         if (i%11 == 0){
             txt = ['Bong'];
         }
+        if (i%17 == 0){
+            txt = txt.reverse();
+        }
         
         
         if (txt.length == 0){
             console.log(i)
         } else {
+            // tostring has commas unfortunately
             var output = '';
             for (var k = 0; k < txt.length; k++){
                 output += txt[k];

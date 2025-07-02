c107e6bf54cd12025b9454c5538d214cdde85726
Shelley Yang <Shelley.Yang@softwire.com>
2025-07-02 10:01:27 +0100

    adding 7

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,16 @@
 // This is our main function
+numbers = [3, 5, 7];
+words = ['Fizz', 'Buzz', 'Bang']
+
 function fizzbuzz() {
     console.log("Hello, World!");
 
     for (var i = 1; i <= 100; i++){
         txt ='';
-        if (i%3 == 0){
-            txt += 'Fizz';
-        }
-        if (i%5 == 0){
-            txt += 'Buzz';
+        for (var j = 0; j < numbers.length; j++){
+            if (i%numbers[j] == 0){
+                txt += words[j];
+            }
         }
         if (txt==''){
             console.log(i)

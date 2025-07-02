c55a35a11f7f6edc21ad0b67ea9aabd358aad032
Shelley Yang <Shelley.Yang@softwire.com>
2025-07-02 10:17:38 +0100

    11 and 13 fixed and hopefully improved

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,27 +1,31 @@
 // This is our main function
-numbers = [3, 5, 7];
-words = ['Fizz', 'Buzz', 'Bang']
+numbers = [3, 13, 5, 7];
+words = ['Fizz', 'Fezz', 'Buzz', 'Bang']
 
 function fizzbuzz() {
     console.log("Hello, World!");
 
-    for (var i = 1; i <= 100; i++){
-        txt ='';
+    for (var i = 1; i <= 105; i++){
+        txt =[];
         for (var j = 0; j < numbers.length; j++){
             if (i%numbers[j] == 0){
-                txt += words[j];
+                txt.push(words[j]);
             }
         }
+
         if (i%11 == 0){
-            txt = 'Bong';
+            txt = ['Bong'];
         }
-        if (i%13 == 0){
-            txt = 'Fezz' + txt;
-        }
-        if (txt==''){
+        
+        
+        if (txt.length == 0){
             console.log(i)
         } else {
-            console.log (txt);
+            var output = '';
+            for (var k = 0; k < txt.length; k++){
+                output += txt[k];
+            }
+            console.log(output);
         }
     }
 
